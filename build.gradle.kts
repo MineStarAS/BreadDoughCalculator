67fb749f03a2640a0e851e75a777888929829752
Project_TL <me@projecttl.net>
2022-06-17 19:19:25 +0900

    feat: migrate Gradle DSL -> Kotlin DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,15 +4,20 @@ buildscript {
         google()
         mavenCentral()
     }
+
     dependencies {
-        classpath "com.android.tools.build:gradle:7.0.0"
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0"
+        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+//task clean(type: Delete) {
+//    deleterootProject.buildDir
+//}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
